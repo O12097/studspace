5501e6132af72ad7b163bf1104f24940fbf1515e
O12097 <desyaderina@gmail.com>
2024-04-04 21:20:01 +0800

    preparation

---
+++ src/components/MySpace.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MySpace = () => {
+    return (
+        <div>
+            <h1>My Space Page</h1>
+        </div>
+    )
+}
---
+++ src/components/Space.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Space = () => {
+    return (
+        <div>
+            <h1>Space Page</h1>
+        </div>
+    )
+}
