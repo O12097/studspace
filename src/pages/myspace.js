75566ac7b2cf19374a1a800cdca128805a55ca8f
O12097 <desyaderina@gmail.com>
2024-04-04 21:38:20 +0800

    several modification in pages

---
+++ src/pages/myspace.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const MySpacePage = () => {
+  return (
+    <div>
+      <h1>My Space Page</h1>
+      <p>This is the My Space page content.</p>
+    </div>
+  )
+}
+
+export default MySpacePage
---
+++ src/pages/space.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const SpacePage = () => {
+  return (
+    <div>
+      <h1>Space Page</h1>
+      <p>This is the space page content.</p>
+    </div>
+  )
+}
+
+export default SpacePage
